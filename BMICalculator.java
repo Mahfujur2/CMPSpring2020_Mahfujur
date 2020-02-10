b4069e01c0c38bc59067301063de1ad63d20e540
Mahfujur2 <60441239+Mahfujur2@users.noreply.github.com>
2020-02-10 12:58:48 -0500

    Add files via upload

---
+++ BMICalculator.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ * @author Mahfujur Rahman
+ * created 2/10/2020
+ * descriptipn: This Program calculates the BMI of 3 Different Users
+ *
+ */
+public class BMICalculator {
+	public static void main(String[]args) {
+		Scanner = scnr new scnr.nextDouble(System.in);
+		
+		System.out.println("This program calculates tje BMI of 3 different users")
+			
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
