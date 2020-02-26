e0b99b8b51b6c2b807e064f4642ecdeb30621210
Mahfujur2 <60441239+Mahfujur2@users.noreply.github.com>
2020-02-26 12:06:06 -0500

    Update NumberGuesser.java

--- NumberGuesser.java
+++ NumberGuesser.java
@@ -4,19 +4,28 @@
  * created 2.24.2020
  * description: Pick a number and ask the user to enter a number, if the user enters THE RIGHT NUMBER say Victory 3 times, if user loses say Game Over 3 times
  */
-import java.util.Random; 
-import java.util.Scanner;
-public class NumberGuesser {
+import java.util.Scanner
+import.java.util.Random;
+public class NumberGuesserCode {
+	public static void main(String[]args)
+		Scanner scnr = new Scanner(System.in);
 	
-	public static void main(String[] args) {
-		Random rand = new Random();
-		int numberToGuess = rand.nextInt(1000);
-		int numberOfTries = 0;
-		Scanner input = new Scanner(System.in);
-		int guess;
+		System.out.println("In this game you are to guess the correct number.")
+		System.out.println("Enter the number you wish to guess between 1 and 20:");
+		int userInput = scnr.nextInt();
+		int rightNum = 20;
 		
-		System.out.println("Guess a number in order to win");
-		
+		if(userInput == rightNum) {
+			system.out.println("Victory! Victory! Victory!");
+			
+		}
+	
+		else {
+			System.out.println("Game Over! Gameover! Game Over!");
+		}
+	
+				  
+	
 		
 	}
 	
