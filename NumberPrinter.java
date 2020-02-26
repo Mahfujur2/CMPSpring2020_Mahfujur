33b93e009788b641273eeb69fa45a4f91eb65d45
Mahfujur2 <60441239+Mahfujur2@users.noreply.github.com>
2020-02-26 12:39:00 -0500

    Add files via upload

---
+++ NumberPrinter.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ * @author Mahfujur Rahman
+ * Created 2.26.2020
+ *
+ */
+import java.util.Scanner;
+public class NumberPrinter {
+	public static void main(String[]args) {
+	for(int i=5;i<=99;i++) {
+		System.out.println(i);
+	}
+	
+	
+}
+}
